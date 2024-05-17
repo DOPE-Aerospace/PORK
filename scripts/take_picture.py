661183236588f167e04332b0d18c02dc9d083ce1
alessandro.trovatello <alessandro.trovatello00@outlook.it>
2024-05-17 16:01:09 +0100

    1920x1080 foto3.jpg

--- scripts/take_picture.py
+++ scripts/take_picture.py
@@ -4,7 +4,8 @@ from picamera2 import Picamera2, Preview
 def scatta_foto(filename):
     camera = Picamera2()
     #camera.resolution = (1024, 768)  # Imposta la risoluzione
-    camera_config = camera.create_preview_configuration()
+    #camera_config = camera.create_preview_configuration()
+    camera_config = camera.create_still_configuration(main={"size": (1920, 1080)})
     camera.configure(camera_config)
     camera.start_preview(Preview.NULL)
     camera.start()
@@ -15,4 +16,4 @@ def scatta_foto(filename):
     camera.close()  # Chiudi la fotocamera
 
 # Utilizzo
-scatta_foto('foto.jpg')
+scatta_foto('foto3.jpg')
