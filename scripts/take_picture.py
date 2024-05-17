436f3940cbba80f1c216c088b50f6e6fdbd0effe
alessandro.trovatello <alessandro.trovatello00@outlook.it>
2024-05-17 15:50:23 +0100

    First picture

--- scripts/take_picture.py
+++ scripts/take_picture.py
@@ -1,12 +1,16 @@
 import time
-from picamera import PiCamera
+from picamera2 import Picamera2, Preview
 
 def scatta_foto(filename):
-    camera = PiCamera()
-    camera.resolution = (1024, 768)  # Imposta la risoluzione
+    camera = Picamera2()
+    #camera.resolution = (1024, 768)  # Imposta la risoluzione
+    camera_config = camera.create_preview_configuration()
+    camera.configure(camera_config)
+    camera.start_preview(Preview.NULL)
+    camera.start()
     #camera.start_preview()  # Avvia l'anteprima
     time.sleep(2)  # Attendi un paio di secondi per permettere alla fotocamera di regolare le impostazioni
-    camera.capture(filename)  # Scatta la foto
+    camera.capture_file(filename)  # Scatta la foto
     #camera.stop_preview()  # Ferma l'anteprima
     camera.close()  # Chiudi la fotocamera
 
