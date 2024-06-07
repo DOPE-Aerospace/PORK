be60277b508c702afc96e3ee409de4d39254eda3
alessandrotrovatello <alessandro.trovatello00@outlook.it>
2024-06-07 15:50:24 +0200

    Created a way to save frames in the 'frames' directory

--- scripts/take_picture.py
+++ scripts/take_picture.py
@@ -1,19 +1,27 @@
 import time
+import os
 from picamera2 import Picamera2
 
+# Define the directory to save the frames
+frames_dir = os.path.join("..", "frames")
+
+# Create the directory if it doesn't exist
+if not os.path.exists(frames_dir):
+    os.makedirs(frames_dir)
+
 def take_photo(filename):
-    picam2 = Picamera2()
-    camera_config = picam2.create_still_configuration(main={"size": (4608, 2592)})
-    picam2.configure(camera_config)
-    picam2.start()
-    picam2.capture_file(filename)  # Take the photo
-    picam2.close()  # Close the camera
+    camera = Picamera2()
+    camera_config = camera.create_still_configuration(main={"size": (4608, 2592)})
+    camera.configure(camera_config)
+    camera.start()
+    camera.capture_file(filename)  # Take the photo
+    camera.close()  # Close the camera
 
 def take_photos_interval(filename_template, interval, num_photos):
     for i in range(num_photos):
-        filename = filename_template.format(i)
+        filename = os.path.join(frames_dir, filename_template.format(i))
         take_photo(filename)
         time.sleep(interval)
 
 # Usage: take a photo every 20 seconds, for a total of 5 photos
-take_photos_interval('frame_{}.jpg', 20, 5)
+take_photos_interval('frames_{}.jpg', 20, 5)
